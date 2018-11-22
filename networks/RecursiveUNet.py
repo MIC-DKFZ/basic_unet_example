e7ebde6315b9fb92d0273d63fa3e4397a85773f4
Andre Klein <andre.klein@dkfz-heidelberg.de>
2018-11-22 12:41:39 +0100

    Added license to some files.
    Signed-off-by: Andre Klein <andre.klein@dkfz-heidelberg.de>

--- networks/RecursiveUNet.py
+++ networks/RecursiveUNet.py
@@ -1,3 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2017 Division of Medical Image Computing, German Cancer Research Center (DKFZ)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Defines the Unet.
 # |num_downs|: number of downsamplings in UNet. For example,
 # if |num_downs| == 7, image of size 128x128 will become of size 1x1 at the bottleneck
