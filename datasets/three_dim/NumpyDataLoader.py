8eabc303307c447105db8c8c65ee6d5b41eafcac
schellem <melanie.schellenberg@dkfz-heidelberg.de>
2020-03-27 17:28:07 +0100

    Basic UNet NumpyDataLoader for multi-channel data

--- datasets/three_dim/NumpyDataLoader.py
+++ datasets/three_dim/NumpyDataLoader.py
@@ -158,10 +158,10 @@ class NumpyDataLoader(SlimDataLoaderBase):
 
             numpy_array = np.load(fn_name, mmap_mode="r")
 
-            data.append(numpy_array[None, self.input[0]])   # 'None' keeps the dimension
+            data.append(numpy_array[list(self.input)])   # 'None' keeps the dimension
 
             if self.label is not None:
-                labels.append(numpy_array[None, self.label[0]])   # 'None' keeps the dimension
+                labels.append(numpy_array[list(self.input)])   # 'None' keeps the dimension
 
             fnames.append(self.files[idx])
             idxs.append(idx)
